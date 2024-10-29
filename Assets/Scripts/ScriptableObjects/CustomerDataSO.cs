0e37c1c8b9dece9d3a67dd622a5308a8b3c4b4a6
jiwonchichi1 <jiwonchichi1@gmail.com>
2024-10-29 22:33:59 +0900

    241029 22:33 스크립터블 오브젝트로 손님 구현

---
+++ Assets/Scripts/ScriptableObjects/CustomerDataSO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Customer Data", menuName = "Scriptable Object/Customer Data", order = 0)]
+public class CustomerDataSO : ScriptableObject
+{
+	public string customerName;
+	public int minItemNum;
+	public int maxItemNum;
+	public Sprite sprite;
+}
