3f1366f5cb6a69bdb0a327abc7ec71e6e4f36143
JiwonEE2 <jiwonchichi1@gmail.com>
2024-10-29 17:40:28 +0900

    241029 17:40 일시정지 구현

--- Assets/Scripts/Systems/UIManager.cs
+++ Assets/Scripts/Systems/UIManager.cs
@@ -7,16 +7,28 @@ public class UIManager : SingletonManager<UIManager>
 {
 	public float money = 0;
 	public TextMeshProUGUI moneyText;
+	public GameObject pausePopup;
 
 	// Start is called before the first frame update
 	void Start()
 	{
-
+		pausePopup.SetActive(false);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
 		moneyText.text = money.ToString();
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			pausePopup.SetActive(true);
+			Time.timeScale = 0;
+		}
+	}
+
+	public void OnClickResumeButton()
+	{
+		pausePopup.SetActive(false);
+		Time.timeScale = 1;
 	}
 }
