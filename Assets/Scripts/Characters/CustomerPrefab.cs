3e0a72fd4b9bd3cdf01c5579271e8d9ca036fae8
JiwonEE2 <jiwonchichi1@gmail.com>
2024-11-08 13:40:53 +0900

    241108 13:40 손님 수정중..

--- Assets/Scripts/Characters/CustomerPrefab.cs
+++ Assets/Scripts/Characters/CustomerPrefab.cs
@@ -24,16 +24,17 @@ public class CustomerPrefab : MonoBehaviour
 	private Sprite foodSprite;
 
 	// 음식을 다 나눠 주었는지 확인을 위함.
-	public bool isGetAllFood = false;
+	public bool isFoodNeed = true;
 	public bool isGoingTable = false;
 	// 테이블에 도착하였는지 확인. 음식을 가지고 있어도 도착한 후에는 스프라이트 표시를 하지 않기 위함.
 	private bool isGetTable = false;
 
 	private bool isEatCoroutineStart = false;
 
+	private bool isEatEnd = false;
+
 	// Customer Controller에서 해당 customer를 삭제하기 위한 변수
 	public bool readyDestroy = false;
-
 	// 충돌 판정을 위함. 바로 다시 false로 돌아가기 때문에 초기화할 필요 없다.
 	private bool isContact = false;
 
@@ -49,20 +50,27 @@ public class CustomerPrefab : MonoBehaviour
 		foodRequireNum = Random.Range(customerData.minFoodNum, customerData.maxFoodNum + 1);
 		spriteRenderer.sortingOrder = 4;
 
-		StartCoroutine(EatCoroutine());
+		isFoodNeed = true;
 	}
 
 	private void Update()
 	{
 		// 1. 밥 받기
-		if (isGetAllFood == false)
+		if (isFoodNeed)
 		{
 			FoodDistribute();
 		}
-		// 2. 빈 테이블 찾고, 3. 돈 내고, 4. 빈 테이블로 이동
+		// 2. 준비된 테이블 찾기
 		else if (isGoingTable == false && TableController.Instance.readyTables.Count > 0)
 		{
+			// 3. 돈 내고, 4. 준비된 테이블로 이동
 			GoEmptyTable();
+			if (isGoingTable == false)
+			{
+				isGoingTable = true;
+				isContact = false;
+				StartCoroutine(MovingCoroutine());
+			}
 		}
 
 		// 음식 가지면 음식 스프라이트 표시하기
@@ -73,12 +81,28 @@ public class CustomerPrefab : MonoBehaviour
 		if (isGoingTable && (Vector2)transform.position == currentTable.GetComponent<TablePrefab>().customerPos)
 		{
 			StopCoroutine(MovingCoroutine());
-			StartEat();
 
+			// 5. 식사
+			StartEat();
+			if (isEatCoroutineStart == false)
+			{
+				isEatCoroutineStart = true;
+				StartCoroutine(EatCoroutine());
+			}
+
+			// 6. 삭제
 			if (foodNum <= 0)
 			{
-				CustomerDestroy();
+				isEatEnd = true;
 			}
+		}
+
+		// 한 번만 실행하면 된다.
+		if (isEatEnd)
+		{
+			isEatEnd = false;
+			StopAllCoroutines();
+			CustomerDestroy();
 		}
 	}
 
@@ -87,7 +111,7 @@ public class CustomerPrefab : MonoBehaviour
 		// 음식량이 충족하는 지 먼저 확인
 		if (foodNum >= foodRequireNum)
 		{
-			isGetAllFood = true;
+			isFoodNeed = false;
 		}
 		// 판매할 수 있을 때. 빌런도 없어야 함
 		else if (GameManager.Instance.isSellingFood && GameManager.Instance.isCounterVillianSpawn == false)
@@ -121,7 +145,6 @@ public class CustomerPrefab : MonoBehaviour
 		// 덜 먹었으면 먹고
 		// 테이블 위에 음식 스프라이트 표시
 		currentTable.GetComponent<TablePrefab>().objectSpriteRenderer.sprite = SpriteManager.Instance.foodSprite;
-		isEatCoroutineStart = true;
 	}
 
 	public void GoEmptyTable()
@@ -133,84 +156,70 @@ public class CustomerPrefab : MonoBehaviour
 			Instantiate(moneyPrefab, moneySpawnPoint, Quaternion.identity);
 		}
 
-		isGoingTable = true;
 		// 준비된 테이블에서 삭제하고 현재 이 손님의 테이블로 설정
 		currentTable = TableController.Instance.readyTables[0];
 		TableController.Instance.readyTables.Remove(currentTable);
-		StartCoroutine(MovingCoroutine());
 	}
 
 	public IEnumerator MovingCoroutine()
 	{
 		// 출발
-		while (isGoingTable)
+		while ((Vector2)transform.position != currentTable.GetComponent<TablePrefab>().customerPos)
 		{
-			// 테이블에 가는 중
-			while ((Vector2)transform.position != currentTable.GetComponent<TablePrefab>().customerPos)
-			{
-				// 1. 출발지에서 목적지까지의 x,y증분을 각각 구한다.
-				float x = currentTable.GetComponent<TablePrefab>().customerPos.x - transform.position.x;
-				float y = currentTable.GetComponent<TablePrefab>().customerPos.y - transform.position.y;
+			// 1. 출발지에서 목적지까지의 x,y증분을 각각 구한다.
+			float x = currentTable.GetComponent<TablePrefab>().customerPos.x - transform.position.x;
+			float y = currentTable.GetComponent<TablePrefab>().customerPos.y - transform.position.y;
 
-				// 충돌하지 않았을 경우
-				if (isContact == false)
+			// 충돌하였을 경우
+			if (isContact)
+			{
+				yield return new WaitForSeconds(1f);
+				isContact = false;
+			}
+			// 충돌하지 않았을 경우
+			else
+			{
+				// 2. 증분이 같아질 때까지 더 큰 증분을 감소시킨다.
+				if (Mathf.Abs(x) > Mathf.Abs(y))
 				{
-					// 2. 증분이 같아질 때까지 더 큰 증분을 감소시킨다.
-					if (Mathf.Abs(x) > Mathf.Abs(y))
+					if (x < 0)
 					{
-						if (x < 0)
-						{
-							transform.Translate(new Vector2(-1, 0));
-							yield return new WaitForSeconds(1 / moveSpeed);
-						}
-						else
-						{
-							transform.Translate(new Vector2(1, 0));
-							yield return new WaitForSeconds(1 / moveSpeed);
-						}
+						transform.Translate(new Vector2(-1, 0));
+						yield return new WaitForSeconds(1 / moveSpeed);
 					}
 					else
 					{
-						if (y < 0)
-						{
-							transform.Translate(new Vector2(0, -1));
-							yield return new WaitForSeconds(1 / moveSpeed);
-						}
-						else
-						{
-							transform.Translate(new Vector2(0, 1));
-							yield return new WaitForSeconds(1 / moveSpeed);
-						}
+						transform.Translate(new Vector2(1, 0));
+						yield return new WaitForSeconds(1 / moveSpeed);
 					}
 				}
-				// 충돌하였을 경우
 				else
 				{
-					yield return new WaitForSeconds(1f);
-					isContact = false;
+					if (y < 0)
+					{
+						transform.Translate(new Vector2(0, -1));
+						yield return new WaitForSeconds(1 / moveSpeed);
+					}
+					else
+					{
+						transform.Translate(new Vector2(0, 1));
+						yield return new WaitForSeconds(1 / moveSpeed);
+					}
 				}
 			}
-
-			yield return null;
 		}
 		yield return null;
 	}
 
 	public IEnumerator EatCoroutine()
 	{
-		while (true)
+		while (foodNum > 0)
 		{
-			if (isEatCoroutineStart && foodNum > 0)
-			{
-				foodNum--;
-				currentTable.GetComponent<TablePrefab>().trashCount++;
-				yield return new WaitForSeconds(1f / GameManager.Instance.eatFoodSpeedPerSeceond[currentTable.GetComponent<TablePrefab>().tableNum]);
-			}
-			else
-			{
-				yield return null;
-			}
+			foodNum--;
+			currentTable.GetComponent<TablePrefab>().trashCount++;
+			yield return new WaitForSeconds(1f / GameManager.Instance.eatFoodSpeedPerSeceond[currentTable.GetComponent<TablePrefab>().tableNum]);
 		}
+		yield return null;
 	}
 
 	public void CustomerDestroy()
@@ -221,7 +230,6 @@ public class CustomerPrefab : MonoBehaviour
 		GameManager.Instance.isCustomerDestoy = true;
 		GameManager.Instance.destroyedCustomerPosition = transform.position;
 		GameManager.Instance.destroyedCustomerTableNum = currentTable.GetComponent<TablePrefab>().tableNum;
-		StopAllCoroutines();
 		readyDestroy = true;
 	}
 
